b9b81e67c165d59ed6b8896ccb12473498cf8cb4
Zongmin Lei <leizongmin@gmail.com>
2014-05-05 16:04:25 +0800

    test before & after

--- test/test.js
+++ test/test.js
@@ -46,4 +46,86 @@ describe('lei-call', function () {
     });
   });
 
+  it('#before', function (done) {
+    var call = createCall();
+    call.register('test_1', function (params, callback) {
+      callback(null, 'test_1');
+    });
+    call.register('test_2', function (params, callback) {
+      params.text += 'test_2';
+      callback(null, params);
+    });
+    call.before('test_2', function (params, callback) {
+      params.is_hook_1 = true;
+      callback();
+    });
+    call.before('test_2', function (params, callback) {
+      params.is_hook_2 = true;
+      callback(null, params);
+    });
+    call.before('test_2', {after: ['ooxx']}, function (params, callback) {
+      params.is_hook_3 = true;
+      params.text += 'A';
+      callback();
+    });
+    call.before('test_2', {name: 'ooxx'}, function (params, callback) {
+      params.is_hook_3 = true;
+      params.text += 'B';
+      callback();
+    });
+    call('test_1', {}, function (err, data) {
+      should.equal(err, null);
+      data.should.equal('test_1');
+      call('test_2', {text: 'XX'}, function (err, data) {
+        should.equal(err, null);
+        should.equal(data.is_hook_1, true);
+        should.equal(data.is_hook_2, true);
+        should.equal(data.is_hook_3, true);
+        should.equal(data.text, 'XXBAtest_2');
+        done();
+      });
+    });
+  });
+
+it('#after', function (done) {
+    var call = createCall();
+    call.register('test_1', function (params, callback) {
+      callback(null, 'test_1');
+    });
+    call.register('test_2', function (params, callback) {
+      params.text += 'test_2';
+      callback(null, params);
+    });
+    call.after('test_2', function (params, callback) {
+      params.is_hook_1 = true;
+      callback();
+    });
+    call.after('test_2', function (params, callback) {
+      params.is_hook_2 = true;
+      callback(null, params);
+    });
+    call.after('test_2', {after: ['ooxx']}, function (params, callback) {
+      params.is_hook_3 = true;
+      params.text += 'A';
+      callback();
+    });
+    call.after('test_2', {name: 'ooxx'}, function (params, callback) {
+      params.is_hook_3 = true;
+      params.text += 'B';
+      callback();
+    });
+    call('test_1', {}, function (err, data) {
+      should.equal(err, null);
+      data.should.equal('test_1');
+      call('test_2', {text: 'XX'}, function (err, data) {
+        should.equal(err, null);
+        should.equal(data.is_hook_1, true);
+        should.equal(data.is_hook_2, true);
+        should.equal(data.is_hook_3, true);
+        should.equal(data.text, 'XXtest_2BA');
+        done();
+      });
+    });
+  });
+
 });
